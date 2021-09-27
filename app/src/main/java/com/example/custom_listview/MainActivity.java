1cc7abe620bb6f581cd066fdfbbf60881c3d2027
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2021-09-27 20:25:54 +0800

    Latihan membuat contact app

---
+++ app/src/main/java/com/example/custom_listview/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.custom_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+
+import com.example.custom_listview.databinding.ActivityMainBinding;
+import com.example.custom_listview.databinding.ActivityUserBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int[] imageId = {
+                R.drawable.indah,
+                R.drawable.yerin,
+                R.drawable.umji
+        };
+
+        String[] name = {
+                "Indah",
+                "Yerin",
+                "Umji"
+        };
+
+        String[] phoneNo = {
+                "0812345678",
+                "0822228888",
+                "0899229929"
+        };
+
+        ArrayList<User> userArrayList = new ArrayList<>();
+
+        for (int i = 0; i < imageId.length; i++) {
+
+            User user = new User(name[i], phoneNo[i], imageId[i]);
+            userArrayList.add(user);
+
+        }
+
+
+        ListAdapter listAdapter = new ListAdapter(MainActivity.this, userArrayList);
+
+        binding.listview.setAdapter(listAdapter);
+        binding.listview.setClickable(true);
+        binding.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(MainActivity.this, UserActivity.class);
+                i.putExtra("name", name[position]);
+                i.putExtra("phone", phoneNo[position]);
+                i.putExtra("imageid", imageId[position]);
+                startActivity(i);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/custom_listview/User.java
@@ -0,0 +1,15 @@
+package com.example.custom_listview;
+
+public class User {
+
+    String name, phoneNo;
+    int imageId;
+
+    public User(String name, String phoneNo, int imageId) {
+        this.name = name;
+        this.phoneNo = phoneNo;
+        this.imageId = imageId;
+    }
+}
+
+
